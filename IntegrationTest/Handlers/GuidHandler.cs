d5b7e74ab8901ca5a1e06bdae650c60ff083636f
atualariao <atualariao.itd.pps@gmail.com>
2022-12-15 11:48:25 +0800

    Minor Updates

--- IntegrationTest/Handlers/GuidHandler.cs
+++ IntegrationTest/Handlers/GuidHandler.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IntegrationTest
+namespace IntegrationTest.Handlers
 {
     class GuidHandler : SqliteTypeHandler<Guid>
     {
--- IntegrationTest/Handlers/MySqlGuidTypeHandler.cs
+++ IntegrationTest/Handlers/MySqlGuidTypeHandler.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IntegrationTest
+namespace IntegrationTest.Handlers
 {
     public class MySqlGuidTypeHandler : SqlMapper.TypeHandler<Guid>
     {
